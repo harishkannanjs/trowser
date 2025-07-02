3d320b0a60485446dd054f7be12cd4743c7a57f7
Harish Kannan J S <harishkannanjs@gmail.com>
2025-07-01 17:16:11 -1200

    Update FAQs.tsx

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -4,27 +4,27 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './
 
 const faqs = [
   {
-    trigger: 'How does Miraweb make browsing faster?  ',
-    content: 'Miraweb is designed for speed, with optimizations that reduce page load times and keep your browsing smooth, even with multiple tabs open.'
+    trigger: "How does Miraweb make browsing faster?",
+    content: "Miraweb is designed for speed, with optimizations that reduce page load times and keep your browsing smooth, even with multiple tabs open."
   },
   {
-    trigger: 'Can I personalize how Miraweb looks and works?',
-    content: 'Yes, Miraweb adapts to your style with customizable themes, layouts, and shortcuts, so your browser feels uniquely yours.'
+    trigger: "Can I personalize how Miraweb looks and works?",
+    content: "Yes, Miraweb adapts to your style with customizable themes, layouts, and shortcuts, so your browser feels uniquely yours."
   },
   {
-    trigger: 'What makes Miraweb more private than other browsers?',
-    content: 'We block invasive trackers by default and never collect your browsing history, ensuring your data stays private and secure.'
+    trigger: "What makes Miraweb more private than other browsers?",
+    content: "We block invasive trackers by default and never collect your browsing history, ensuring your data stays private and secure."
   },
   {
-    trigger: 'How does the AI in Miraweb help me?',
-    content: 'Miraweb’s AI simplifies tasks—like finding information or organizing tabs—by learning your habits and anticipating what you need.'
+    trigger: "How does the AI in Miraweb help me?",
+    content: "Miraweb&apos;s AI simplifies tasks—like finding information or organizing tabs—by learning your habits and anticipating what you need."
   },
   {
-    trigger: 'Is Miraweb good for gaming or streaming?',
-    content: 'Absolutely. Miraweb enhances gaming and video streaming with smoother performance and fewer interruptions.'
+    trigger: "Is Miraweb good for gaming or streaming?",
+    content: "Absolutely. Miraweb enhances gaming and video streaming with smoother performance and fewer interruptions."
   },
   {
-    trigger: 'How often does Miraweb update?',
+    trigger: "How often does Miraweb update?",
     content: "We regularly improve Miraweb with new features and security updates, keeping your browsing experience fresh and protected."
   }
 ]
