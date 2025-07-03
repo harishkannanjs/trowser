9ecd58c2e6cbde7a81d616a9470e09b41f737e27
Harish Kannan J S <harishkannanjs@gmail.com>
2025-07-03 22:09:34 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -83,7 +83,7 @@ const Navbar = () => {
       >
         <div className='flex items-center gap-16 text-neutral-400'>
           <div className='flex items-center'>
-           <div className='h-24 w-24 rounded-full'>
+           <div className='h-32 w-32 rounded-full'>
               <Image
                 src={'/Trowser_Logo.webp'}
                 height={200}
